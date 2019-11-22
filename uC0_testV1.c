2fd6c1fa8e9c07096a7084125378bacaa4a09b40
ahung005 <ahung005@ucr.edu>
2019-11-21 19:48:07 -0800

    created uC0 test file

---
+++ uC0_testV1.c
@@ -0,0 +1,122 @@
+
+/*
+ * uC01_testV1.c
+ *
+ * Created: 11/17/2019 6:26:02 PM
+ * Author : aahun
+ */ 
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include "usart_ATmega1284.h"
+
+void ADC_init() {
+	
+	ADMUX = (1<<REFS1) | (1<<REFS0) ;
+	ADCSRA|= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	//        in Free Running Mode, a new conversion will trigger whenever
+	//        the previous conversion completes.
+}
+uint16_t analog_read(uint8_t channel)
+{
+	ADMUX = (ADMUX & 0xF8) | (channel & 7);
+	
+	ADCSRA|=(1<<ADSC);
+	while ( !(ADCSRA & (1<<ADIF)));
+	ADCSRA|=(1<<ADIF);
+	
+	
+	return (ADC);
+}
+
+
+
+
+int main(void) {
+	DDRA = 0x00;	PORTA = 0xFF;
+	DDRB = 0xFF;	PORTB = 0x00;
+	DDRC = 0xFF;	PORTC = 0x00;
+	
+	int x;
+	int y;
+	char bufferx[20];
+	char buffery[20];
+	
+	ADC_init();
+	initUSART(0);
+	//LCD_init();
+	//LCD_ClearScreen();
+	
+    /* Replace with your application code */
+    while (1) {
+		x = analog_read(0);
+		//LCD_ClearScreen();
+		//sprintf(bufferx, "X=%d   ", x);
+		//LCD_DisplayString(1, bufferx);
+		
+		y = analog_read(1);
+		//sprintf(buffery, "Y=%d   ", y);
+		//LCD_DisplayString(8, buffery);
+		
+		if((x < 800 && x > 300) && (y < 800 && y > 300)) {
+			//LCD_DisplayString(17, "center");	
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x00, 0);
+			}
+		}
+		else if(x > 800 && (y < 800 && y > 300)) {
+			//LCD_DisplayString(17, "front");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x01, 0);
+			}
+		}
+		else if(x > 800 && y < 300) {
+			//LCD_DisplayString(17, "left-front");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x02, 0);
+			}
+		}
+		else if(x > 800 && y > 800) {
+			//LCD_DisplayString(17, "right-front");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x03, 0);
+			}
+		}
+		else if((x < 800 && x > 300) && y < 100) {
+			//LCD_DisplayString(17, "left");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x04, 0);
+			}
+		}
+		else if((x < 800 && x > 300) && y > 800) {
+			//LCD_DisplayString(17, "right");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x05, 0);
+			}
+		}
+		else if(x < 300 && y < 300) {
+			//LCD_DisplayString(17, "left-rear");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x10, 0);
+			}
+		}
+		else if(x < 300 && y > 800) {
+			//LCD_DisplayString(17, "right-rear");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x20, 0);
+			}
+		}
+		else if(x < 300 && (y < 800 && y > 300)) {
+			//LCD_DisplayString(17, "rear");
+			if(USART_IsSendReady(0)) {
+				USART_Send(0x30, 0);
+			}
+		}
+		//delay_ms(200);
+    }
+}
